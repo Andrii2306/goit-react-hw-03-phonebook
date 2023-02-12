d064d715ccfd2673abad4f8d6099fc5512caa1d6
Andrii2306 <andriy06b@gmail.com>
2023-02-12 11:42:50 +0200

    1

--- src/components/Section/Section.jsx
+++
@@ -1,9 +0,0 @@
-import css from '../Section/Section.module.css';
-import PropTypes from 'prop-types';
-
-export const Section = ({ children }) => {
-  return <div className={css.section}>{children}</div>;
-};
-Section.propTypes = {
-  children: PropTypes.array.isRequired,
-};
